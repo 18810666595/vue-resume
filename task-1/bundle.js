18d1a0a7172978296acd83712077abd3e897f452
cg <654450188@qq.com>
2017-05-30 00:33:33 +0800

    resume step 2

--- task-1/bundle.js
+++ task-1/bundle.js
@@ -68,26 +68,33 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* harmony export (immutable) */ __webpack_exports__["a"] = bar;
-function bar() {
-  alert('Hello Webpack!')
-}
 
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = bar;
+function bar() {
+  alert('Hello Webpack!');
+}
 
 /***/ }),
 /* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__bar__ = __webpack_require__(0);
 
 
-__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__bar__["a" /* default */])();
+var _bar = __webpack_require__(0);
 
+var _bar2 = _interopRequireDefault(_bar);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _bar2.default)();
 
 /***/ })
 /******/ ]);
